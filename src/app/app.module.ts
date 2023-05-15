971f3f12165cc7416876633cae9f0dd0f7a7bac3
Abraham Zambrano <abrahamzf2000@hotmail.com>
2023-05-15 17:24:07 +0200

    Making some components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PruebaComponent } from './Componentes/prueba.component';
+import { Componente2Component } from './componente2/componente2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, PruebaComponent, Componente2Component
   ],
   imports: [
     BrowserModule
